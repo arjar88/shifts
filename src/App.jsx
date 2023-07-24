50fa2a84858236038b3131b8708f9f64239e8f68
arjar88 <aryehakierman@gmail.com>
2023-07-24 15:38:11 +0300

    used grid for app layout,for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
 import Workers from "./components/Workers/Workers";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
-    <div style={{ height: "100%", display: "flex" }}>
-      <Menu></Menu>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/workers" element={<Workers />}></Route>
-      </Routes>
+    <div>
+      <Grid container>
+        <Grid item xs={2}>
+          <Menu></Menu>
+        </Grid>
+
+        <Grid item xs={10}>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/workers" element={<Workers />}></Route>
+          </Routes>
+        </Grid>
+      </Grid>
     </div>
   );
 }
