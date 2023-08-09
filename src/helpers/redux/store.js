b27dcb30a569d55b285f4ebab22269708ccc7e69
arjar88 <superaryeh98@gmail.com>
2023-08-09 16:44:01 +0300

    actually added selectedWorker slice lol

--- src/helpers/redux/store.js
+++ src/helpers/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import workersReducer from "../redux/slices/workers";
+import selectedWorkerReducer from "./slices/selectedWorker";
 
 export default configureStore({
   reducer: {
     workers: workersReducer,
+    selectedWorker: selectedWorkerReducer,
   },
 });
