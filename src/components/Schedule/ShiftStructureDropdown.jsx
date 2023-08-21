c9a3bb9cc34b396603ae3851171b25729d3d4656
arjar88 <superaryeh98@gmail.com>
2023-08-21 15:39:07 +0300

    updates to ShiftStructureDropdown

--- src/components/Schedule/ShiftStructureDropdown.jsx
+++ src/components/Schedule/ShiftStructureDropdown.jsx
@@ -1,5 +1,6 @@
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
 const ShiftStructureDropdown = () => {
@@ -7,12 +8,27 @@ const ShiftStructureDropdown = () => {
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+
+  const styles = {
+    textFieldStyle: {
+      width: "12em",
+    },
+  };
   return (
-    <Select value={age} placeholder="Number Of Shifts" onChange={handleChange}>
-      <MenuItem value={10}>Ten</MenuItem>
-      <MenuItem value={20}>Twenty</MenuItem>
-      <MenuItem value={30}>Thirty</MenuItem>
-    </Select>
+    <>
+      <TextField
+        sx={styles.textFieldStyle}
+        variant="outlined"
+        value={age}
+        onChange={handleChange}
+        select
+        label="Shift Structuresdddddddddddddddddddd"
+      >
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>vdvdvdvdvdv</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </TextField>
+    </>
   );
 };
 
